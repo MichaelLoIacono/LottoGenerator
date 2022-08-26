1ddfd1e7559d1c0f6da38e479be3d5801c757c89
michaelloiacono <fragmentox88@gmail.com>
2022-08-26 11:13:04 +0200

    Initial Commit

---
+++ LottoGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoGenerator
+//
+//  Created by Michael Lo iacono on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
